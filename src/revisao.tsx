00366a45161b05e9a3a02c2a562fdd1b661f81d7
Gustavo Silva <gustavo.silva20@estudante.ifms.edu.br>
2024-06-24 11:34:10 -0400

    Update revisao.tsx

--- src/revisao.tsx
+++ src/revisao.tsx
@@ -1,15 +1,15 @@
 import { ChangeEvent, useState } from "react";
 
-function Revisao():JSX.Element{
-    const [a,setA] = useState(0)
-    const [b,setB] = useState(0)
-    function soma(a:number,b:number):number{
-        return a+b
+function Revisao(): JSX.Element {
+    const [a, setA] = useState(0)
+    const [b, setB] = useState(0)
+    function soma(a: number, b: number): number {
+        return a + b
     }
-    function trataInput1(e:ChangeEvent<HTMLInputElement>){
+    function trataInput1(e: ChangeEvent<HTMLInputElement>) {
         setA(Number(e.target.value))
     }
-    function trataInput2(e:ChangeEvent<HTMLInputElement>){
+    function trataInput2(e: ChangeEvent<HTMLInputElement>) {
         setB(Number(e.target.value))
     }
 
@@ -19,25 +19,25 @@ function Revisao():JSX.Element{
     //EX: listaNumeros(10,15) 
     //retorno "10,11,12,13,14,15,"
 
-    function listaNumeros(a1:number,b1:number):string{
-        let lista:string = ""
+    function listaNumeros(a1: number, b1: number): string {
+        let lista: string = ""
         //Fazer um processamento dos números
         //let str = "10,"
         let i = a1
-        while(i<=b1){
-            lista = lista.concat(String(i)+",")
-            i++
+        while (i >= b1) {
+            lista = lista.concat(String(i) + ",")
+            i--
         }
         return lista
     }
 
 
-    return( 
+    return (
         <>
             <h1>Componente</h1>
-            <input type="number" onChange={trataInput1}/>
-            <input type="number" onChange={trataInput2}/>
-            <h1>{listaNumeros(a,b)}</h1>
+            <input type="number" onChange={trataInput1} />
+            <input type="number" onChange={trataInput2} />
+            <h1>{listaNumeros(a, b)}</h1>
         </>
     )
 }
